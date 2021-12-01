5203780694113a1d9980a0a655a221015890f0b1
AbdelHameed-Hamed <Abdelhameed.h.h@gmail.com>
2021-12-01 14:28:32 +0200

    Some naming changes and cleanup.

--- assets/shaders/ocean.comp.mesh.frag.hlsl
+++ assets/shaders/ocean.comp.mesh.frag.hlsl
@@ -25,22 +25,23 @@ Texture2D<float4> waves: register(t0, space1);
 // This is because it first computes the FFT for every row, then the FFT for every column, which means
 // we might have a data race if we're not careful.
 
-// x: height, y: unused, zw: partial derivatives of height with respect to x and z
-RWTexture2D<float4> height_output_input: register(u1, space1);
-RWTexture2D<float4> height_input_output: register(u2, space1);
+// x: height, y: unused, zw: displacement in the xz directions
+RWTexture2D<float4> displacement_output_input: register(u1, space1);
+RWTexture2D<float4> displacement_input_output: register(u2, space1);
 
-// xy: displacement in x and z directions, z: partial derivative of x's displacement with respect to x
+// xy: partial derivatives of the height with resepect to x and z
+// z: partial derivative of x's displacement with respect to x
 // w: partial derivative of z's displacement with respect to z
-RWTexture2D<float4> displacement_output_input: register(u3, space1);
-RWTexture2D<float4> displacement_input_output: register(u4, space1);
+RWTexture2D<float4> derivatives_output_input: register(u3, space1);
+RWTexture2D<float4> derivatives_input_output: register(u4, space1);
 
 //------------------------------------------------------------------------------------------------------
 // Compute Shader
 //------------------------------------------------------------------------------------------------------
 
 // Based on https://github.com/asylum2010/Asylum_Tutorials/blob/master/Media/ShadersGL/fourier_fft.comp
-groupshared float4 height_pingpong[2][OCEAN_DIM];
 groupshared float4 displacement_pingpong[2][OCEAN_DIM];
+groupshared float4 derivatives_pingpong[2][OCEAN_DIM];
 
 [[vk::push_constant]]
 struct {
@@ -58,12 +59,6 @@ void cs_main(uint x: SV_GroupThreadID, uint z: SV_GroupID) {
 
         float w_k_t = sqrt(9.81 * length(k)) * fog_distances.z;
 
-        float2 d = k;
-        if (abs(d.x - ((OCEAN_DIM >> 1) * TWO_PI / fog_distances.y)) <= 0.00001)
-            d.x = 0;
-        if (abs(d.y - ((OCEAN_DIM >> 1) * TWO_PI / fog_distances.y)) <= 0.00001)
-            d.y = 0;
-
         // Calculate tilde_h at time t
         float2 tilde_h1 = waves[loc1].xy;
         float2 tilde_h2 = complex_conjugate(waves[loc2].xy);
@@ -72,31 +67,30 @@ void cs_main(uint x: SV_GroupThreadID, uint z: SV_GroupID) {
             complex_mul(tilde_h1, complex_exp(w_k_t)) +
             complex_mul(tilde_h2, complex_exp(-w_k_t));
 
-        height_pingpong[0][x].xy = tilde_h_t;
-
-        // Calculate the partial derivatives of the height with respect to x and z at time t
-        height_pingpong[0][x].zw = complex_mul(d.yx * float2(-1, 1), tilde_h_t);
-
-        // Calculate the xz displacement at time t
-        float2 d_t = complex_mul(d.yx / length(k) * float2(1, -1), tilde_h_t);
+        float2 d_t = complex_mul(k.yx / length(k) * float2(1, -1), tilde_h_t);
         if (length(k) <= 0.00001)
             d_t = float2(0, 0);
 
-        displacement_pingpong[0][x].xy = d_t;
+        // Calculate the displacement in the xyz directions
+        displacement_pingpong[0][x].xy = tilde_h_t;
+        displacement_pingpong[0][x].zw = d_t;
 
-        // Calculate the partial derivatives of the displacement with respect to x and z at time t
-        displacement_pingpong[0][x].zw = complex_mul(d.yx * float2(-1, 1), d_t);
+        // Calculate the partial derivatives of height with respect to xz at time t
+        derivatives_pingpong[0][x].xy = complex_mul(k.yx * float2(-1, 1), tilde_h_t);
+
+        // Calculate the partial derivatives of xz displacements with respect to xz at time t
+        derivatives_pingpong[0][x].zw = complex_mul(k.yx * float2(-1, 1), d_t);
     }
 
     // Do IFFT
     // STEP 1: load row/column and reorder
     int nj = (reversebits(x) >> (32 - OCEAN_DIM_EXPONENT)) & (OCEAN_DIM - 1);
     if (flags.flags.x == 0) {
-        height_pingpong[1][nj] = height_pingpong[0][x];
         displacement_pingpong[1][nj] = displacement_pingpong[0][x];
+        derivatives_pingpong[1][nj] = derivatives_pingpong[0][x];
     } else {
-        height_pingpong[1][nj] = height_output_input[uint2(x, z)];
         displacement_pingpong[1][nj] = displacement_output_input[uint2(x, z)];
+        derivatives_pingpong[1][nj] = derivatives_output_input[uint2(x, z)];
     }
 
     GroupMemoryBarrierWithGroupSync();
@@ -105,40 +99,40 @@ void cs_main(uint x: SV_GroupThreadID, uint z: SV_GroupID) {
     int src = 1;
 
     for (int s = 1; s <= OCEAN_DIM_EXPONENT; ++s) {
-        int m = 1L << s;            // butterfly group height
-        int mh = m >> 1;            // butterfly group half height
+        int m = 1L << s;            // butterfly group displacement
+        int mh = m >> 1;            // butterfly group half displacement
 
         if (x % m < mh) {
             // Twiddle factor W_N^k
             float2 w_n_k = complex_exp(TWO_PI * x / m);
 
             // Height calculations
-            float2 even = height_pingpong[src][x].xy;
-            float2 odd = complex_mul(w_n_k, height_pingpong[src][x + mh].xy);
-
-            height_pingpong[1 - src][x].xy = even + odd;
-            height_pingpong[1 - src][x + mh].xy = even - odd;
-
-            // Partial derivative of height with respect to x and z
-            even = height_pingpong[src][x].zw;
-            odd = complex_mul(w_n_k, height_pingpong[src][x + mh].zw);
-
-            height_pingpong[1 - src][x].zw = even + odd;
-            height_pingpong[1 - src][x + mh].zw = even - odd;
-
-            // Displacement calculations
-            even = displacement_pingpong[src][x].xy;
-            odd = complex_mul(w_n_k, displacement_pingpong[src][x + mh].xy);
+            float2 even = displacement_pingpong[src][x].xy;
+            float2 odd = complex_mul(w_n_k, displacement_pingpong[src][x + mh].xy);
 
             displacement_pingpong[1 - src][x].xy = even + odd;
             displacement_pingpong[1 - src][x + mh].xy = even - odd;
 
-            // Partial derivative of displacement with respect to x and z
+            // xz displacement calculations
             even = displacement_pingpong[src][x].zw;
             odd = complex_mul(w_n_k, displacement_pingpong[src][x + mh].zw);
 
             displacement_pingpong[1 - src][x].zw = even + odd;
             displacement_pingpong[1 - src][x + mh].zw = even - odd;
+
+            // Partial derivative of height with respect to xz
+            even = derivatives_pingpong[src][x].xy;
+            odd = complex_mul(w_n_k, derivatives_pingpong[src][x + mh].xy);
+
+            derivatives_pingpong[1 - src][x].xy = even + odd;
+            derivatives_pingpong[1 - src][x + mh].xy = even - odd;
+
+            // Partial derivative of xz displacement with respect to xz
+            even = derivatives_pingpong[src][x].zw;
+            odd = complex_mul(w_n_k, derivatives_pingpong[src][x + mh].zw);
+
+            derivatives_pingpong[1 - src][x].zw = even + odd;
+            derivatives_pingpong[1 - src][x + mh].zw = even - odd;
         }
 
         src = 1 - src;
@@ -149,15 +143,15 @@ void cs_main(uint x: SV_GroupThreadID, uint z: SV_GroupID) {
     // STEP 3: write output
     // Transpose the image
     uint2 idx = {z, x};
-    float4 height_result = height_pingpong[src][x];
     float4 displacement_result = displacement_pingpong[src][x];
+    float4 derivatives_result = derivatives_pingpong[src][x];
     if (flags.flags.x == 0) {
-        height_output_input[idx] = height_result;
         displacement_output_input[idx] = displacement_result;
+        derivatives_output_input[idx] = derivatives_result;
     } else {
         float sign_correction = ((x + z) & 1) == 1 ? -1 : 1;
-        height_input_output[idx] = height_result * sign_correction;
-        displacement_input_output[idx] = displacement_result * sign_correction;
+        displacement_input_output[idx] = (displacement_result * sign_correction).zxwy;
+        derivatives_input_output[idx] = derivatives_result * sign_correction;
     }
 }
 
@@ -211,14 +205,17 @@ void ms_main(
                 mul(projection, view),
                 float4(
                     global_x + u * displacement_input_output[global_idx].x,
-                    height_input_output[global_idx].x * 15,
-                    global_z + u * displacement_input_output[global_idx].y,
+                    displacement_input_output[global_idx].y * 15,
+                    global_z + u * displacement_input_output[global_idx].z,
                     1
                 )
             );
 
             float3 normal = float3(
-                -(height_input_output[global_idx].zw / (1 + u * displacement_input_output[global_idx].zw)),
+                -(
+                    derivatives_input_output[global_idx].xy /
+                    (1 + u * derivatives_input_output[global_idx].zw)
+                ),
                 1
             ).xzy;
             out_verts[vert_idx].normal = float4(normalize(normal), 1);
